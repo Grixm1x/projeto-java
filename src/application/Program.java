a66ee90f5d2e616504cbb76114faf3ea0b4b99ac
Joao Marcelus <joaomarcelus6@gmail.com>
2025-01-22 17:47:01 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,21 @@
 package application;
 
-import java.util.Date;
+import java.sql.SQLException;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-	
-		Department obj = new Department(1, "Books");
+	public static void main(String[] args) throws SQLException {
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println("=== Test number 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
+
 		System.out.println(seller);
-		
 
 	}
 
